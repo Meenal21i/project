0a35611cbed2df95fba0ad94cae35906f17f4440
Meenal Sharma <meenal.sharma@intimetec.com>
2025-07-07 00:04:06 +0530

    removed duplicacy & used common method

--- hrms-playwright/pages/LeaveAttendancePage.ts
+++ hrms-playwright/pages/LeaveAttendancePage.ts
@@ -8,32 +8,36 @@ export class LeaveAttendancePage extends NavigationPage {
     super(page);
   }
 
-  async navigateToAttendanceRecord(): Promise<void> {
+  private async expandLeaveAndAttendanceSection(): Promise<void> {
     await this.wrapper.click(NavBarLocators['Leave & Attendance']);
+  }
+
+  async navigateToAttendanceRecord(): Promise<void> {
+    await this.expandLeaveAndAttendanceSection();
     await this.wrapper.click(NavBarLocators.AttendanceRecord);
     await this.page.waitForURL(PageUrls.ATTENDANCE_RECORD);
   }
 
   async navigateToLeavesApplication(): Promise<void> {
-    await (NavBarLocators['Leave & Attendance']);
+    await this.expandLeaveAndAttendanceSection();
     await this.wrapper.click(NavBarLocators.LeavesApplication);
     await this.page.waitForURL(PageUrls.LEAVES_APPLICATION);
   }
 
   async navigateToLeaveEntitlements(): Promise<void> {
-    await this.wrapper.click(NavBarLocators['Leave & Attendance']);
+    await this.expandLeaveAndAttendanceSection();
     await this.wrapper.click(NavBarLocators.LeaveEntitlements);
     await this.page.waitForURL(PageUrls.LEAVE_ENTITLEMENTS);
   }
 
   async navigateToLeaveCorrection(): Promise<void> {
-    await this.wrapper.click(NavBarLocators['Leave & Attendance']);
+    await this.expandLeaveAndAttendanceSection();
     await this.wrapper.click(NavBarLocators.LeaveCorrection);
     await this.page.waitForURL(PageUrls.LEAVE_CORRECTION);
   }
 
   async navigateToMyHolidays(): Promise<void> {
-    await this.wrapper.click(NavBarLocators['Leave & Attendance']);
+    await this.expandLeaveAndAttendanceSection();
     await this.wrapper.click(NavBarLocators.MyHolidays);
     await this.page.waitForURL(PageUrls.MY_HOLIDAYS);
   }
