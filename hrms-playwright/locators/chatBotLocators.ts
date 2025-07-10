40fec3320dc7f44fc36d34a0ff5306caeca6f07a
Meenal Sharma <meenal.sharma@intimetec.com>
2025-07-10 22:49:53 +0530

    updated some chatbot & login locators

--- hrms-playwright/locators/chatBotLocators.ts
+++ hrms-playwright/locators/chatBotLocators.ts
@@ -3,7 +3,7 @@ export const ChatBotLocators = {
   CHATBOT_BOX: '.mantine-hyj2ep',
   HEADER_TEXT_1: 'div.mantine-sep1nv',
   HEADER_TEXT_2: 'div.mantine-sm8m4w',
-  MESSAGE_INPUT: 'input.mantine-Input-input.mantine-15c3rd2',
-  SUBMIT_BUTTON: 'button[type="submit"][data-button="true"]',
+  MESSAGE_INPUT: 'input[placeholder="Type a message..."]',
+  SUBMIT_BUTTON: 'button[type="submit"]',
   SENT_MESSAGE: '.mantine-l8szka'
 };
--- hrms-playwright/locators/loginLocators.ts
+++ hrms-playwright/locators/loginLocators.ts
@@ -1,5 +1,5 @@
 export const LoginLocators = {
-  ERROR_MESSAGE: '.mantine-6l7pzb.mantine-Notification-body',
+  ERROR_MESSAGE: '.mantine-Notification-body',
   LOGIN_BUTTON: 'button[id="login-form-input-submit-btn"]',
   PASSWORD_INPUT: '#login-form-input-password',
   USERNAME_INPUT: '#login-form-input-username',
